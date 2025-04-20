1bb45b6c9be471598fd68fb5cca06d5abbd78366
fl-armable <fl.irlanda@gmail.com>
2025-04-20 21:42:31 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,16 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  //arrays con elementos
+let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+let action = ['ate', 'peed', 'crushed', 'broke'];
+let what = ['my homework', 'my phone', 'the car'];
+let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+  //eligiendo elementos aleatoriamente
+let rndm1 = who[Math.floor(Math.random()*who.length)] + " ";
+let rndm2 = action[Math.floor(Math.random()*action.length)] + " ";
+let rndm3 = what[Math.floor(Math.random()*what.length)] + " ";
+let rndm4 = when[Math.floor(Math.random()*when.length)] + ".";
+  //enviando al HTML mediante id = excuse
+  document.querySelector("#excuse").innerHTML = rndm1 + rndm2 + rndm3 + rndm4;
 };
